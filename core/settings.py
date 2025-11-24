4d92e90669895b9ae37492b5f760afaadc1a5a39
lidia.qama <lidia.qama@italiacoding.com>
2025-11-24 13:06:57 +0100

    allow host

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["todo-be-1-glf7.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
