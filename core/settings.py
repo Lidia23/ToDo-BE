174c96eee85b4a533c8dc01ef25793f2d7d7e6f1
lidia.qama <lidia.qama@italiacoding.com>
2025-11-24 20:16:51 +0100

    back to sqlite3

--- core/settings.py
+++ core/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': dj_database_url.config(
-    #     default=f"sqlite:///{BASE_DIR / 'db.postgresql'}"
-    # )
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
     
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
@@ -98,10 +98,10 @@ DATABASES = {
     #     'PORT':'5432'
     # }
 
-    'default': dj_database_url.config(
-        default='postgresql://admin:cD535KEGx7gR9gQnOyl3nj65vKnscVky@dpg-d4iadv75r7bs73b98rd0-a.frankfurt-postgres.render.com/tododb_grxd',
-        conn_max_age=600
-    )
+    # 'default': dj_database_url.config(
+    #     default='postgresql://admin:cD535KEGx7gR9gQnOyl3nj65vKnscVky@dpg-d4iadv75r7bs73b98rd0-a.frankfurt-postgres.render.com/tododb_grxd',
+    #     conn_max_age=600
+    # )
 }
 
 
